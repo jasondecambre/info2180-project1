ce3bb82d77020feadd4963f4f62a7f90695a709d
Jason DeCambre <jasondecambre@gmail.com>
2021-10-17 16:16:26 -0500

    Should be about everything

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 /* Add your JavaScript to this file */
 window.onload = function(){
-    
+    var input = document.getElementsByTagName("input")[0];
+    var button = document.getElementsByTagName("button")[0];
+    var message = document.getElementsByClassName("message")[0];
+    button.addEventListener("click", function(event){
+        event.preventDefault();
+        if((input.value === "")||(input.value.indexOf("@") === -1) || (input.value.indexOf(".") === -1)){
+            message.innerHTML = "Please enter a valid email address.";
+        }
+        else{
+            message.innerHTML = "Thank you! Your email address " + input.textContent + " has ben added to our mailing list!";
+        }
+    });
+
 }
+
+//
